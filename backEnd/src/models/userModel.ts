605f1d376db63a0891f22c0be526c016a9d11a84
nakharin aroonyadej <nakharin_aroon@kkumail.com>
2022-04-18 16:45:10 +0700

    [change] Branch Be to branch Dev structure

--- backEnd/src/models/userModel.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-// create an schema
-var userSchema = new mongoose.Schema({
-  name:String,
-  age:Number,
-});
-
-export const userModel = mongoose.model("userModel", userSchema) 
